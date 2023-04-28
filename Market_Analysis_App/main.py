5fd7ba200384594e8d0636ed20561e65a23f2dea
MABINOGIONII\Azusa.Chiaki <tomoe.teatime@gmail.com>
2023-04-28 04:19:12 -0400

    Enhanced Chart Img display with enlarge function by one click.

--- Market_Analysis_App/main.py
+++ Market_Analysis_App/main.py
@@ -142,7 +142,7 @@ class MovingAverage(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -191,7 +191,7 @@ class BollingerBands(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -259,7 +259,7 @@ class ADX(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -306,7 +306,7 @@ class VWAP(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -362,7 +362,7 @@ class StochasticOscillator(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -426,7 +426,7 @@ class RSI(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -492,7 +492,7 @@ class MADC(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -568,7 +568,7 @@ class FibonacciRetracement(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -615,7 +615,7 @@ class OBV(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -652,7 +652,7 @@ class AccumulationDistributionLine(Stock):
         plt.savefig(plt_buffer, format='png')
         plt_buffer.seek(0)
         plt_base64 = b64encode(plt_buffer.read()).decode('utf-8')
-        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class='chart-img'></a>"
+        plt_html = f"<img src='data:image/png;base64,{plt_base64}' class="chart-img" onclick="enlargeImage(this)" ondblclick="downsizeImage(this)"></a>"
         plt.close()
         return plt_html
 
@@ -1045,6 +1045,16 @@ def generate_html_end():
               evt.currentTarget.className += " active";
           }}
           document.getElementById("defaultOpen").click();
+          
+        function enlargeImage(img) {{
+            img.style.width = "50%";
+            img.style.height = "50%";
+        }}
+
+        function downsizeImage(img) {{
+            img.style.width = "25%";
+            img.style.height = "25%";
+        }}
       </script>
       </body>
     </html>
